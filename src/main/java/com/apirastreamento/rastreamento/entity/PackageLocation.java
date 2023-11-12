341339d03af34706f9470ef2d58d1a5314c23cfb
LUAN WILLIAM <luanwma@hotmail.com>
2023-11-11 22:38:42 -0300

    subindo classes event

---
+++ src/main/java/com/apirastreamento/rastreamento/entity/PackageLocation.java
@@ -0,0 +1,33 @@
+package com.apirastreamento.rastreamento.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PackageLocation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String city;
+
+    private String state;
+
+    private String zipCode;
+
+    private String country;
+
+
+}
